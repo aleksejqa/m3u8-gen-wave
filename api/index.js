5be341d72535a1f35b6b594bba4109382e771b29
West <anton.zakharov@wdg.com.ua>
2023-02-17 21:56:07 +0200

    try promise

--- api/index.js
+++ api/index.js
@@ -15,8 +15,8 @@ app.get('/api', async (req,res) => {
 
     res.send(`<div><div>${time}</div><ul><li><a href="/api/update">Update</a></li><li><a href="/public/west.m3u">get</a></li></ul></div>`)
 })
-app.get('/api/update', (req, res) => {
-    getData();
+app.get('/api/update', async (req, res) => {
+    await getData();
     res.send('<div><button onclick="window.location.href=`/`; return false;">back</button><span>updating...</span></div>');
 })
 app.use(express.static('public'))
